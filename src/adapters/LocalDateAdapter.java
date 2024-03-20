ddf5188d8f276310f003e45471e9e3a35ed1d61d
Mikhail Viktorov <the-rayn@yandex.ru>
2024-03-20 15:36:41 +0300

    fix: codeStyle

--- src/adapters/LocalDateAdapter.java
+++ src/adapters/LocalDateAdapter.java
@@ -7,15 +7,15 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
-    DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
 
     @Override
     public LocalDateTime deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-        return LocalDateTime.parse(jsonElement.getAsString(), FORMATTER);
+        return LocalDateTime.parse(jsonElement.getAsString(), formatter);
     }
 
     @Override
     public JsonElement serialize(LocalDateTime localDateTime, Type type, JsonSerializationContext jsonSerializationContext) {
-        return new JsonPrimitive(localDateTime.format(FORMATTER));
+        return new JsonPrimitive(localDateTime.format(formatter));
     }
 }
